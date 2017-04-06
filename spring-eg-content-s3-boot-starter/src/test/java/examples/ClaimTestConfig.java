0677bd81f60747e656f8822b8e75abecd11d707a
Paul Warren <paul.warren@emc.com>
2017-04-05 22:40:29 -0700

    update s3 examples for new placement service

--- spring-eg-content-s3-boot-starter/src/test/java/examples/ClaimTestConfig.java
+++ spring-eg-content-s3-boot-starter/src/test/java/examples/ClaimTestConfig.java
@@ -2,11 +2,24 @@ package examples;
 
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+
+import internal.org.springframework.content.commons.placementstrategy.UUIDPlacementStrategy;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.aws.core.io.s3.SimpleStorageResourceLoader;
+import org.springframework.content.commons.placementstrategy.PlacementStrategy;
 import org.springframework.content.s3.config.AbstractS3ContentRepositoryConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ClaimTestConfig extends AbstractS3ContentRepositoryConfiguration {
+
+	@Autowired
+	private AmazonS3 client;
 
     @Override
     public String bucket() {
@@ -18,4 +31,20 @@ public class ClaimTestConfig extends AbstractS3ContentRepositoryConfiguration {
         return Region.getRegion(Regions.fromName(System.getenv("AWS_REGION")));
     }
 
+    @Override
+	public SimpleStorageResourceLoader simpleStorageResourceLoader() {
+		client.setRegion(region());
+		return new SimpleStorageResourceLoader(client);
+	}
+
+	@Bean
+	public PlacementStrategy<String> placementStrategy() {
+		return new PlacementStrategy<String>() {
+
+			@Override
+			public String getLocation(String contentId) {
+				return new UUIDPlacementStrategy().getLocation(UUID.fromString(contentId));
+			}
+		};
+	}
 }
--- spring-eg-content-s3/src/test/java/examples/ClaimTestConfig.java
+++ spring-eg-content-s3/src/test/java/examples/ClaimTestConfig.java
@@ -1,11 +1,13 @@
 package examples;
 
 
+import java.util.UUID;
+
 import javax.sql.DataSource;
 
-import com.amazonaws.regions.RegionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.aws.context.config.annotation.EnableContextResourceLoader;
+import org.springframework.cloud.aws.core.io.s3.SimpleStorageResourceLoader;
+import org.springframework.content.commons.placementstrategy.PlacementStrategy;
 import org.springframework.content.s3.config.AbstractS3ContentRepositoryConfiguration;
 import org.springframework.content.s3.config.EnableS3ContentRepositories;
 import org.springframework.context.annotation.Bean;
@@ -23,7 +25,10 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
+import com.amazonaws.regions.RegionUtils;
+import com.amazonaws.services.s3.AmazonS3;
+
+import internal.org.springframework.content.commons.placementstrategy.UUIDPlacementStrategy;
 
 @Configuration
 @ComponentScan
@@ -35,6 +40,25 @@ public class ClaimTestConfig extends AbstractS3ContentRepositoryConfiguration {
 
 	@Autowired
 	private Environment env;
+	
+	@Autowired
+	private AmazonS3 client;
+
+	@Override
+	public String bucket() {
+		return env.getProperty("AWS_BUCKET");
+	}
+	
+	@Override
+	public Region region() {
+		return RegionUtils.getRegion(env.getProperty("AWS_REGION"));
+	}
+	
+	@Override
+	public SimpleStorageResourceLoader simpleStorageResourceLoader() {
+		client.setRegion(region());
+		return new SimpleStorageResourceLoader(client);
+	}
 
 	@Bean
 	public DataSource dataSource() {
@@ -65,14 +89,14 @@ public class ClaimTestConfig extends AbstractS3ContentRepositoryConfiguration {
 		return txManager;
 	}
 
-	@Override
-	public String bucket() {
-		return env.getProperty("AWS_BUCKET");
-	}
+	@Bean
+	public PlacementStrategy<String> placementStrategy() {
+		return new PlacementStrategy<String>() {
 
-	@Override
-	public Region region() {
-		return RegionUtils.getRegion(env.getProperty("AWS_REGION"));
+			@Override
+			public String getLocation(String contentId) {
+				return new UUIDPlacementStrategy().getLocation(UUID.fromString(contentId));
+			}
+		};
 	}
-
 }
