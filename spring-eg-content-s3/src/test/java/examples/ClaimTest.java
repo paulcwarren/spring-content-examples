1d15c9a0b8c0aa2cfc236ce3a71d64c4b0ac3637
Jeff Pak <jeffrey.pak@emc.com>
2017-04-07 14:02:17 -0700

    add backwards compatibility test for s3 [#140515205](https://www.pivotaltracker.com/story/show/140515205)

--- spring-eg-content-s3/src/test/java/examples/ClaimTest.java
+++ spring-eg-content-s3/src/test/java/examples/ClaimTest.java
@@ -1,14 +1,65 @@
 package examples;
 
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.aws.core.io.s3.SimpleStorageResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.WritableResource;
 import org.springframework.test.context.ContextConfiguration;
 
 import com.github.paulcwarren.ginkgo4j.Ginkgo4jConfiguration;
 import com.github.paulcwarren.ginkgo4j.Ginkgo4jSpringRunner;
+
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.util.UUID;
+
 
 @RunWith(Ginkgo4jSpringRunner.class)
 @Ginkgo4jConfiguration(threads=1)
 @ContextConfiguration(classes = { ClaimTestConfig.class })
 public class ClaimTest extends AbstractSpringContentTests {
 
+    @Autowired
+    private ClaimTestConfig config;
+
+    {
+        Describe("Spring Content Amazon S3", () -> {
+            Describe("Backwards Compatibility", () -> {
+                Context("given content in the old placement location", () -> {
+                    BeforeEach(() -> {
+                        // write some content in the old placement location (store root)
+                        // and create a entity so we can try and fetch it
+                        String contentId = UUID.randomUUID().toString();
+                        SimpleStorageResourceLoader storageResourceLoader = config.simpleStorageResourceLoader();
+
+                        Resource resource = storageResourceLoader.getResource("s3://" + config.bucket() + "/" + contentId);
+                        if (resource instanceof WritableResource) {
+                            OutputStream out = ((WritableResource)resource).getOutputStream();
+                            out.write("Hello Content World!".getBytes());
+                            out.close();
+                        }
+                        ClaimForm claimForm = new ClaimForm();
+                        claimForm.setContentId(contentId);
+                        claimForm.setContentLength(20L);
+                        claimForm.setMimeType("text/plain");
+
+                        claim = new Claim();
+                        claim.setFirstName("John");
+                        claim.setLastName("Smith");
+                        claim.setClaimForm(claimForm);
+                        claimRepo.save(claim);
+                    });
+                    It("getContent will find it", () -> {
+                        assertThat(IOUtils.contentEquals(claimFormStore.getContent(claim.getClaimForm()), IOUtils.toInputStream("Hello Content World!", Charset.defaultCharset())), is(true));
+                    });
+                });
+            });
+        });
+    }
 }
