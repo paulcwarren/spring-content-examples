231c0de795606e929f0e349ea2dddc2c555613a1
Paul Warren <paul.warren@emc.com>
2017-04-21 15:00:02 -0700

    More unused imports

--- spring-eg-content-fs-boot-starter/src/main/java/examples/Application.java
+++ spring-eg-content-fs-boot-starter/src/main/java/examples/Application.java
@@ -4,13 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
-import org.springframework.content.commons.placement.PlacementStrategy;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import internal.org.springframework.content.commons.placement.UUIDPlacementStrategy;
-
-import java.util.UUID;
 
 /**
  * exclude={MongoRepositoriesAutoConfiguration.class} is only required because we re-use
