15c3682693934d6b94fd5882b746cc4c40284dff
Paul Warren <paul.warren@emc.com>
2018-05-23 09:48:54 -0700

    Renditions examples; Import boot 2.0 localserverport

--- spring-eg-content-renditions/src/test/java/api/rest/RenditionRestTest.java
+++ spring-eg-content-renditions/src/test/java/api/rest/RenditionRestTest.java
@@ -7,9 +7,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.jayway.restassured.RestAssured;
