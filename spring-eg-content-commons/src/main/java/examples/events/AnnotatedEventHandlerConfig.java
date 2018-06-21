46003f27a2c8077edf5e664840bb63a49f5d41e6
Paul Warren <paul.warren@emc.com>
2018-06-21 10:05:56 -0700

    Add tests for the event variant of annotated event handling

--- spring-eg-content-commons/src/main/java/examples/events/AnnotatedEventHandlerConfig.java
+++ spring-eg-content-commons/src/main/java/examples/events/AnnotatedEventHandlerConfig.java
@@ -14,139 +14,131 @@ import org.springframework.content.commons.annotations.HandleBeforeSetContent;
 import org.springframework.content.commons.annotations.HandleBeforeUnassociate;
 import org.springframework.content.commons.annotations.HandleBeforeUnsetContent;
 import org.springframework.content.commons.annotations.StoreEventHandler;
+import org.springframework.content.commons.repository.events.AfterAssociateEvent;
+import org.springframework.content.commons.repository.events.AfterGetContentEvent;
+import org.springframework.content.commons.repository.events.AfterGetResourceEvent;
+import org.springframework.content.commons.repository.events.AfterSetContentEvent;
+import org.springframework.content.commons.repository.events.AfterUnassociateEvent;
+import org.springframework.content.commons.repository.events.AfterUnsetContentEvent;
+import org.springframework.content.commons.repository.events.BeforeAssociateEvent;
+import org.springframework.content.commons.repository.events.BeforeGetContentEvent;
+import org.springframework.content.commons.repository.events.BeforeGetResourceEvent;
+import org.springframework.content.commons.repository.events.BeforeSetContentEvent;
+import org.springframework.content.commons.repository.events.BeforeUnassociateEvent;
+import org.springframework.content.commons.repository.events.BeforeUnsetContentEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import examples.models.ClaimForm;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 
 @Configuration
 public class AnnotatedEventHandlerConfig {
 	
 	@Bean
 	public ExampleAnnotatedEventHandler eventHandler() {
-		return new ExampleAnnotatedEventHandler();
+		return mock(ExampleAnnotatedEventHandler.class);
 	}
 	
 	@StoreEventHandler
 	public static class ExampleAnnotatedEventHandler {
 
-		private int beforeGetResource = 0;
-		private int afterGetResource = 0;
-		private int beforeAssociate = 0;
-		private int afterAssociate = 0;
-		private int beforeUnassociate = 0;
-		private int afterUnassociate = 0;
-		private int beforeSet = 0;
-		private int afterSet = 0;
-		private int beforeGet = 0;
-		private int afterGet = 0;
-		private int beforeUnset = 0;
-		private int afterUnset = 0;
-
 		@HandleBeforeGetResource
 		public void handleBeforeGetResource(Document doc) {
-			beforeGetResource++;
 		}
 
-		public int handleBeforeGetResourceCallCount() {
-			return beforeGetResource;
+		@HandleBeforeGetResource
+		public void handleBeforeGetResourceEvent(BeforeGetResourceEvent event) {
 		}
 
 		@HandleAfterGetResource
 		public void handleAfterGetResource(Document doc) {
-			afterGetResource++;
 		}
 
-		public int handleAfterGetResourceCallCount() { return afterGetResource; }
+		@HandleAfterGetResource
+		public void handleAfterGetResourceEvent(AfterGetResourceEvent event) {
+		}
 
 		@HandleBeforeAssociate
 		public void handleBeforeAssociate(Document doc) {
-			beforeAssociate++;
 		}
 
-		public int handleBeforeAssociateCallCount() {
-			return beforeAssociate;
+		@HandleBeforeAssociate
+		public void handleBeforeAssociateEvent(BeforeAssociateEvent event) {
 		}
 
 		@HandleAfterAssociate
 		public void handleAfterAssociate(Document doc) {
-			afterAssociate++;
 		}
 
-		public int handleAfterAssociateCallCount() {
-			return afterAssociate;
+		@HandleAfterAssociate
+		public void handleAfterAssociateEvent(AfterAssociateEvent event) {
 		}
 
 		@HandleBeforeUnassociate
 		public void handleBeforeUnassociate(Document doc) {
-			beforeUnassociate++;
 		}
 
-		public int handleBeforeUnassociateCallCount() {
-			return beforeUnassociate;
+		@HandleBeforeUnassociate
+		public void handleBeforeUnassociateEvent(BeforeUnassociateEvent event) {
 		}
 
 		@HandleAfterUnassociate
 		public void handleAfterUnassociate(Document doc) {
-			afterUnassociate++;
 		}
 
-		public int handleAfterUnassociateCallCount() {
-			return afterUnassociate;
+		@HandleAfterUnassociate
+		public void handleAfterUnassociateEvent(AfterUnassociateEvent event) {
 		}
 
 		@HandleBeforeSetContent
 		public void handleBeforeSetContent(ClaimForm claim) {
-			beforeSet++;
 		}
-		
-		public int handleBeforeSetCallCount() {
-			return beforeSet;
+
+		@HandleBeforeSetContent
+		public void handleBeforeSetContentEvent(BeforeSetContentEvent event) {
 		}
-		
+
 		@HandleAfterSetContent
 		public void handleAfterSetContent(ClaimForm claim) {
-			afterSet++;
 		}
-		
-		public int handleAfterSetCallCount() {
-			return afterSet;
+
+		@HandleAfterSetContent
+		public void handleAfterSetContentEvent(AfterSetContentEvent event) {
 		}
 
 		@HandleBeforeGetContent
 		public void handleBeforeGetContent(ClaimForm claim) {
-			beforeGet++;
 		}
-		
-		public int handleBeforeGetCallCount() {
-			return beforeGet;
+
+		@HandleBeforeGetContent
+		public void handleBeforeGetContentEvent(BeforeGetContentEvent event) {
 		}
-		
+
 		@HandleAfterGetContent
 		public void handleAfterGetContent(ClaimForm claim) {
-			afterGet++;
 		}
-		
-		public int handleAfterGetCallCount() {
-			return afterGet;
+
+		@HandleAfterGetContent
+		public void handleAfterGetContentEvent(AfterGetContentEvent event) {
 		}
-	
+
 		@HandleBeforeUnsetContent
 		public void handleBeforeUnsetContent(ClaimForm claim) {
-			beforeUnset++;
 		}
-		
-		public int handleBeforeUnsetCallCount() {
-			return beforeUnset;
+
+		@HandleBeforeUnsetContent
+		public void handleBeforeUnsetContentEvent(BeforeUnsetContentEvent event) {
 		}
-		
+
 		@HandleAfterUnsetContent
 		public void handleAfterUnsetContent(ClaimForm claim) {
-			afterUnset++;
 		}
-		
-		public int handleAfterUnsetCallCount() {
-			return afterUnset;
+
+		@HandleAfterUnsetContent
+		public void handleAfterUnsetContentEvent(AfterUnsetContentEvent event) {
 		}
 	}
 }
--- spring-eg-content-commons/src/main/java/examples/events/AnnotatedEventHandlerStoreTests.java
+++ spring-eg-content-commons/src/main/java/examples/events/AnnotatedEventHandlerStoreTests.java
@@ -1,15 +1,35 @@
 package examples.events;
 
 import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.Describe;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.FIt;
 import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.It;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.hamcrest.MockitoHamcrest.argThat;
 
+import examples.models.Claim;
+import examples.models.ClaimForm;
+import examples.models.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import examples.ContentStoreTests;
 import examples.events.AnnotatedEventHandlerConfig.ExampleAnnotatedEventHandler;
+import org.springframework.content.commons.repository.events.AfterAssociateEvent;
+import org.springframework.content.commons.repository.events.AfterGetContentEvent;
+import org.springframework.content.commons.repository.events.AfterGetResourceEvent;
+import org.springframework.content.commons.repository.events.AfterSetContentEvent;
+import org.springframework.content.commons.repository.events.AfterUnassociateEvent;
+import org.springframework.content.commons.repository.events.AfterUnsetContentEvent;
+import org.springframework.content.commons.repository.events.BeforeAssociateEvent;
+import org.springframework.content.commons.repository.events.BeforeGetContentEvent;
+import org.springframework.content.commons.repository.events.BeforeGetResourceEvent;
+import org.springframework.content.commons.repository.events.BeforeSetContentEvent;
+import org.springframework.content.commons.repository.events.BeforeUnassociateEvent;
+import org.springframework.content.commons.repository.events.BeforeUnsetContentEvent;
 
 public class AnnotatedEventHandlerStoreTests extends ContentStoreTests {
 	
@@ -18,40 +38,76 @@ public class AnnotatedEventHandlerStoreTests extends ContentStoreTests {
 	{
 		Describe("Annotated Event Handler", () -> {
 			It("should have called BeforeGetResource", () -> {
-				assertThat(eventHandler.handleBeforeGetResourceCallCount(), is(greaterThan(0)));
+				verify(eventHandler, atLeastOnce()).handleBeforeGetResource(argThat(instanceOf(Document.class)));
+			});
+			It("should have called BeforeGetResourceEvent", () -> {
+				verify(eventHandler, atLeastOnce()).handleBeforeGetResourceEvent(argThat(instanceOf(BeforeGetResourceEvent.class)));
 			});
 			It("should have called AfterGetResource", () -> {
-				assertThat(eventHandler.handleAfterGetResourceCallCount(), is(greaterThan(0)));
+				verify(eventHandler, atLeastOnce()).handleAfterGetResource(argThat(instanceOf(Document.class)));
+			});
+			It("should have called AfterGetResourceEvent", () -> {
+				verify(eventHandler, atLeastOnce()).handleAfterGetResourceEvent(argThat(instanceOf(AfterGetResourceEvent.class)));
 			});
 			It("should have called BeforeAssociate", () -> {
-				assertThat(eventHandler.handleBeforeAssociateCallCount(), is(greaterThan(0)));
+				verify(eventHandler, atLeastOnce()).handleBeforeAssociate(argThat(instanceOf(Document.class)));
+			});
+			It("should have called BeforeAssociateEvent", () -> {
+				verify(eventHandler, atLeastOnce()).handleBeforeAssociateEvent(argThat(instanceOf(BeforeAssociateEvent.class)));
 			});
 			It("should have called AfterAssociate", () -> {
-				assertThat(eventHandler.handleAfterAssociateCallCount(), is(greaterThan(0)));
+				verify(eventHandler, atLeastOnce()).handleAfterAssociate(argThat(instanceOf(Document.class)));
+			});
+			It("should have called AfterAssociateEvent", () -> {
+				verify(eventHandler, atLeastOnce()).handleAfterAssociateEvent(argThat(instanceOf(AfterAssociateEvent.class)));
 			});
 			It("should have called BeforeUnassociate", () -> {
-				assertThat(eventHandler.handleBeforeUnassociateCallCount(), is(greaterThan(0)));
+				verify(eventHandler, atLeastOnce()).handleBeforeUnassociate(argThat(instanceOf(Document.class)));
+			});
+			It("should have called BeforeUnassociateEvent", () -> {
+				verify(eventHandler, atLeastOnce()).handleBeforeUnassociateEvent(argThat(instanceOf(BeforeUnassociateEvent.class)));
 			});
 			It("should have called AfterAssociate", () -> {
-				assertThat(eventHandler.handleAfterUnassociateCallCount(), is(greaterThan(0)));
+				verify(eventHandler, atLeastOnce()).handleAfterUnassociate(argThat(instanceOf(Document.class)));
+			});
+			It("should have called AfterAssociateEvent", () -> {
+				verify(eventHandler, atLeastOnce()).handleAfterUnassociateEvent(argThat(instanceOf(AfterUnassociateEvent.class)));
 			});
 			It("should have called BeforeSetContent", () -> {
-				assertThat(eventHandler.handleBeforeSetCallCount(), is(greaterThan(0)));
+				verify(eventHandler, atLeastOnce()).handleBeforeSetContent(argThat(instanceOf(ClaimForm.class)));
+			});
+			It("should have called BeforeSetContentEvent", () -> {
+				verify(eventHandler, atLeastOnce()).handleBeforeSetContentEvent(argThat(instanceOf(BeforeSetContentEvent.class)));
 			});
 			It("should have called AfterSetContent", () -> {
-				assertThat(eventHandler.handleAfterSetCallCount(), is(greaterThan(0)));
+				verify(eventHandler, atLeastOnce()).handleAfterSetContent(argThat(instanceOf(ClaimForm.class)));
+			});
+			It("should have called AfterSetContentEvent", () -> {
+				verify(eventHandler, atLeastOnce()).handleAfterSetContentEvent(argThat(instanceOf(AfterSetContentEvent.class)));
 			});
 			It("should have called BeforeGetContent", () -> {
-				assertThat(eventHandler.handleBeforeGetCallCount(), is(greaterThan(0)));
+				verify(eventHandler, atLeastOnce()).handleBeforeGetContent(argThat(instanceOf(ClaimForm.class)));
+			});
+			It("should have called BeforeGetContentEvent", () -> {
+				verify(eventHandler, atLeastOnce()).handleBeforeGetContentEvent(argThat(instanceOf(BeforeGetContentEvent.class)));
 			});
 			It("should have called AfterGetContent", () -> {
-				assertThat(eventHandler.handleAfterGetCallCount(), is(greaterThan(0)));
+				verify(eventHandler, atLeastOnce()).handleAfterGetContent(argThat(instanceOf(ClaimForm.class)));
+			});
+			It("should have called AfterGetContentEvent", () -> {
+				verify(eventHandler, atLeastOnce()).handleAfterGetContentEvent(argThat(instanceOf(AfterGetContentEvent.class)));
 			});
 			It("should have called BeforeUnsetContent", () -> {
-				assertThat(eventHandler.handleBeforeUnsetCallCount(), is(greaterThan(0)));
+				verify(eventHandler, atLeastOnce()).handleBeforeUnsetContent(argThat(instanceOf(ClaimForm.class)));
+			});
+			It("should have called BeforeUnsetContentEvent", () -> {
+				verify(eventHandler, atLeastOnce()).handleBeforeUnsetContentEvent(argThat(instanceOf(BeforeUnsetContentEvent.class)));
 			});
 			It("should have called AfterUnsetContent", () -> {
-				assertThat(eventHandler.handleAfterUnsetCallCount(), is(greaterThan(0)));
+				verify(eventHandler, atLeastOnce()).handleAfterUnsetContent(argThat(instanceOf(ClaimForm.class)));
+			});
+			It("should have called AfterUnsetContentEvent", () -> {
+				verify(eventHandler, atLeastOnce()).handleAfterUnsetContentEvent(argThat(instanceOf(AfterUnsetContentEvent.class)));
 			});
 		});
 	}
