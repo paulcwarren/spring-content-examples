232b1a7c7963c0d6eb40e2dd25e0e634d2edb9c0
Paul Warren <paul.warren@emc.com>
2019-03-08 09:14:56 -0800

    Add LockingAndVersioningRepository package to @Enable...Repositories
            - see DATACMNS-1487 - Dont cache candidate bean definitions

--- spring-eg-content-hypermedia/src/main/java/examples/versioning/Application.java
+++ spring-eg-content-hypermedia/src/main/java/examples/versioning/Application.java
@@ -27,13 +27,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.versions.jpa.config.JpaLockingAndVersioningConfig;
 
-import javax.sql.DataSource;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-
 @SpringBootApplication
-@EnableJpaRepositories(basePackages={"tests.versioning","internal.org.springframework.versions.jpa"})
+@EnableJpaRepositories(basePackages={"tests.versioning","org.springframework.versions"})
 @EnableTransactionManagement
 @EnableFilesystemStores(basePackages="tests.versioning")
 @Import(JpaLockingAndVersioningConfig.class)
