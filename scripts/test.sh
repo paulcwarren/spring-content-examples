0c5891d25b4a84aa2c43f44cd4fea85032df7eed
Paul Warren <paul.warren@emc.com>
2019-09-24 07:24:44 -0700

    Add a script to make testing individual projects easier

---
+++ scripts/test.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -eu
+
+function usage() {
+  cat <<EOT
+Usage: $(basename "$0") <options>
+Options:
+  -a  Test entire project
+  -s  Test project identified by suffix (\`-s commons\` tests spring-content-commons)
+EOT
+}
+
+function test() {
+  
+  if [[ -z "${1}" ]]; then
+		BUILD_TYPE=dev mvn clean install     
+  elif [[ "${1}" == "all" ]]; then
+		BUILD_TYPE=dev mvn clean install     
+  else
+		BUILD_TYPE=dev mvn -pl "spring-eg-content-${1}" -am clean install
+  fi
+
+}
+
+function main() {
+  local o
+
+  while getopts as:h o; do
+      case $o in
+        a)
+          test "all"
+          ;;
+        s)
+          test "${OPTARG}"
+          ;;
+        *)
+          echo "Invalid flag: ${o}"
+          usage
+          exit 1
+      esac
+    done
+
+   shift $((OPTIND-1))
+}
+
+main "$@"
