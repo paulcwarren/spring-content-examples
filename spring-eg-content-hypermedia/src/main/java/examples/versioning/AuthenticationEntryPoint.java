d3708cbb8e7ad82a268ce3c0856bf4c7da70cd95
Paul Warren <paul.warren@emc.com>
2019-12-08 13:27:40 +0000

    Bump to Spring Boot 2.2.2 and Spring Cloud Hoxton

--- spring-eg-content-hypermedia/src/main/java/examples/versioning/AuthenticationEntryPoint.java
+++ spring-eg-content-hypermedia/src/main/java/examples/versioning/AuthenticationEntryPoint.java
@@ -1,21 +1,21 @@
 package examples.versioning;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 @Component
 public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
-            throws IOException, ServletException {
+            throws IOException {
         response.addHeader("WWW-Authenticate", "Basic realm=" +getRealmName());
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         PrintWriter writer = response.getWriter();
