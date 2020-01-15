757ca7f55fa31478b413a07bdb0870525b29a335
Paul Warren <paul.warren@emc.com>
2020-01-14 17:32:23 -0800

    Refactor ContentStoreTests

--- spring-eg-content-commons/src/main/java/tests/smoke/ContentStoreTests.java
+++ spring-eg-content-commons/src/main/java/tests/smoke/ContentStoreTests.java
@@ -25,43 +25,23 @@ import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 
 public abstract class ContentStoreTests extends AssociativeStoreTests {
-	
+
 	@Autowired protected ClaimRepository claimRepo;
 	@Autowired protected ClaimFormStore claimFormStore;
 
 	protected Claim claim;
-    protected Object id;
-    
-    {
-		Describe("ContentStore", () -> {
-			
-			AfterEach(() -> {
-				// delete any existing claim forms
-				Iterable<Claim> existingClaims = claimRepo.findAll();
-				for (Claim existingClaim : existingClaims) {
-					if (existingClaim.getClaimForm() != null && claimFormStore.getContent(existingClaim.getClaimForm()) != null) {
-                        String contentId = existingClaim.getClaimForm().getContentId();
-                        claimFormStore.unsetContent(existingClaim.getClaimForm());
-                        if (existingClaim.getClaimForm() != null) {
-                            Assert.assertThat(existingClaim.getClaimForm().getContentId(), is(nullValue()));
-                            Assert.assertEquals(existingClaim.getClaimForm().getContentLength(), 0);
+	protected Object id;
 
-                            // double check the content got removed
-                            ClaimForm deletedClaimForm = new ClaimForm();
-                            deletedClaimForm.setContentId(contentId);
-                            InputStream content = claimFormStore.getContent(deletedClaimForm);
-                            Assert.assertThat(content, is(nullValue()));
-                            IOUtils.closeQuietly(content);
-                        }
-                    }
-				}
-				
-				// delete existing claims
-				claimRepo.deleteAll();
+	{
+		Describe("ContentStore", () -> {
+
+			AfterEach(() -> {
+				deleteAllClaimFormsContent();
+				deleteAllClaims();
 			});
-			
+
 			Context("given an Entity with content", () -> {
-				
+
 				BeforeEach(() -> {
 					claim = new Claim();
 					claim.setFirstName("John");
@@ -70,7 +50,7 @@ public abstract class ContentStoreTests extends AssociativeStoreTests {
 					claimFormStore.setContent(claim.getClaimForm(), new ByteArrayInputStream("Hello Spring Content World!".getBytes()));
 					claim = claimRepo.save(claim);
 				});
-				
+
 				It("should be able to store new content", () -> {
 					boolean matches = false;
 					InputStream content = null;
@@ -83,19 +63,19 @@ public abstract class ContentStoreTests extends AssociativeStoreTests {
 					}
 					assertThat(matches, is(true));
 				});
-				
+
 				It("should have content metadata", () -> {
 					Assert.assertThat(claim.getClaimForm().getContentId(), is(notNullValue()));
 					Assert.assertThat(claim.getClaimForm().getContentId().trim().length(), greaterThan(0));
 					Assert.assertEquals(claim.getClaimForm().getContentLength(), 27L);
 				});
-				
+
 				Context("when content is updated", () -> {
 					BeforeEach(() ->{
 						claimFormStore.setContent(claim.getClaimForm(), new ByteArrayInputStream("Hello Updated Spring Content World!".getBytes()));
 						claim = claimRepo.save(claim);
 					});
-					
+
 					It("should have the updated content", () -> {
 						boolean matches = false;
 						InputStream content = null;
@@ -109,23 +89,23 @@ public abstract class ContentStoreTests extends AssociativeStoreTests {
 						assertThat(matches, is(true));
 					});
 				});
-				
+
 				Context("when content is deleted", () -> {
-				    BeforeEach(() -> {
-                        id = claim.getClaimForm().getContentId();
+					BeforeEach(() -> {
+						id = claim.getClaimForm().getContentId();
 						claimFormStore.unsetContent(claim.getClaimForm());
 						claim = claimRepo.save(claim);
 					});
 
-				    AfterEach(() -> {
-				        claimRepo.delete(claim);
-                    });
-					
-					It("should have no content", () -> {
-                        ClaimForm deletedClaimForm = new ClaimForm();
-                        deletedClaimForm.setContentId((String)id);
+					AfterEach(() -> {
+						claimRepo.delete(claim);
+					});
 
-                        InputStream content = claimFormStore.getContent(deletedClaimForm);
+					It("should have no content", () -> {
+						ClaimForm deletedClaimForm = new ClaimForm();
+						deletedClaimForm.setContentId((String)id);
+
+						InputStream content = claimFormStore.getContent(deletedClaimForm);
 						Assert.assertThat(content, is(nullValue()));
 						IOUtils.closeQuietly(content);
 
@@ -135,8 +115,53 @@ public abstract class ContentStoreTests extends AssociativeStoreTests {
 				});
 			});
 		});
-    }
-    
+	}
+
+	protected boolean hasContent(ClaimForm claimForm) {
+
+		if (claimForm == null) {
+			return false;
+		}
+
+		try (InputStream content = claimFormStore.getContent(claimForm)) {
+			if (content != null) {
+				return true;
+			}
+		} catch (IOException e) {
+		}
+
+		return false;
+	}
+
+	protected void deleteAllClaims() {
+		claimRepo.deleteAll();
+	}
+
+	protected void deleteAllClaimFormsContent() {
+		Iterable<Claim> existingClaims = claimRepo.findAll();
+		for (Claim existingClaim : existingClaims) {
+			if (existingClaim.getClaimForm() != null && hasContent(existingClaim.getClaimForm())) {
+				String contentId = existingClaim.getClaimForm().getContentId();
+				claimFormStore.unsetContent(existingClaim.getClaimForm());
+				if (existingClaim.getClaimForm() != null) {
+					Assert.assertThat(existingClaim.getClaimForm().getContentId(), is(nullValue()));
+					Assert.assertEquals(existingClaim.getClaimForm().getContentLength(), 0);
+
+					// double check the content got removed
+					ClaimForm deletedClaimForm = new ClaimForm();
+					deletedClaimForm.setContentId(contentId);
+					InputStream content = claimFormStore.getContent(deletedClaimForm);
+					try {
+						Assert.assertThat(content, is(nullValue()));
+					}
+					finally {
+						IOUtils.closeQuietly(content);
+					}
+				}
+			}
+		}
+	}
+
 	@Test
 	public void noop() throws IOException {
 	}
