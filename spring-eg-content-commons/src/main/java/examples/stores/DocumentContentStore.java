efefdaf06f8da2ff5688505b2aaae448055c29d5
Paul Warren <paul.warren@emc.com>
2020-01-14 17:12:36 -0800

    Fix compilation error

--- spring-eg-content-commons/src/main/java/examples/stores/DocumentContentStore.java
+++ spring-eg-content-commons/src/main/java/examples/stores/DocumentContentStore.java
@@ -1,7 +1,9 @@
 package examples.stores;
 
 import examples.models.Document;
-import org.springframework.content.commons.repository.AssociativeStore;
 
-public interface DocumentAssociativeStore extends AssociativeStore<Document, String> {
+import org.springframework.content.commons.repository.AssociativeStore;
+import org.springframework.content.commons.repository.ContentStore;
+
+public interface DocumentContentStore extends ContentStore<Document, String> {
 }
