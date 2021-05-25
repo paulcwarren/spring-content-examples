4ce175004685423b9923b4962f94b88d55ffcd59
Paul Warren <paul_c_warren@yahoo.com>
2021-05-24 22:06:00 -0700

    Use test containers

--- spring-eg-content-elasticsearch/src/test/java/examples/ElasticsearchConfig.java
+++ spring-eg-content-elasticsearch/src/test/java/examples/ElasticsearchConfig.java
@@ -2,10 +2,7 @@ package examples;
 
 import javax.sql.DataSource;
 
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.content.elasticsearch.EnableElasticsearchFulltextIndexing;
 import org.springframework.content.jpa.config.EnableJpaStores;
@@ -65,7 +62,7 @@ public class ElasticsearchConfig {
 
     @Bean
     public RestHighLevelClient client() {
-        return new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
+        return ElasticsearchTestContainer.client();
     }
 
     @Value("/org/springframework/content/jpa/schema-drop-hsqldb.sql")
