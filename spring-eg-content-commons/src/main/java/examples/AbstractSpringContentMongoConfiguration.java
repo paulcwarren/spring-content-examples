f93bc769f569a08096fadf9eebbf9f01f1ae1f12
Paul Warren <paul_c_warren@yahoo.com>
2021-05-24 22:50:03 -0700

    Use test containers

--- spring-eg-content-commons/src/main/java/examples/AbstractSpringContentMongoConfiguration.java
+++ spring-eg-content-commons/src/main/java/examples/AbstractSpringContentMongoConfiguration.java
@@ -1,10 +1,7 @@
 package examples;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
@@ -13,22 +10,22 @@ import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 
+import com.mongodb.client.MongoClient;
+
 @Configuration
 public class AbstractSpringContentMongoConfiguration extends AbstractMongoClientConfiguration {
 
 	private static final Log log = LogFactory.getLog(AbstractSpringContentMongoConfiguration.class);
 
-	@Value("#{environment.MONGODB_URL}")
-	private String mongoDbUrl = "mongodb://localhost:27017";
-
 	@Override
 	protected String getDatabaseName() {
-		return "spring-content";
+        return MongoTestContainer.getTestDbName();
 	}
 
-	@Bean
+	@Override
+    @Bean
 	public MongoClient mongoClient() {
-		return MongoClients.create(mongoDbUrl);
+        return MongoTestContainer.getMongoClient();
 	}
 
 	@Bean
@@ -36,7 +33,8 @@ public class AbstractSpringContentMongoConfiguration extends AbstractMongoClient
 		return new GridFsTemplate(mongoDbFactory(), mongoConverter);
 	}
 
-	@Bean
+	@Override
+    @Bean
 	public MongoDatabaseFactory mongoDbFactory() {
 		return new SimpleMongoClientDatabaseFactory(mongoClient(), getDatabaseName());
 	}
