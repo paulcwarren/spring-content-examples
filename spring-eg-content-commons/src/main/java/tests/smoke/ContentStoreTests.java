2f3be8bdbff9158dd29b8ef27d4ae9ab7b9fae46
Paul Warren <paul_c_warren@yahoo.com>
2022-02-03 20:45:58 -0800

    Refactor tests to use new property path

--- spring-eg-content-commons/src/main/java/tests/smoke/ContentStoreTests.java
+++ spring-eg-content-commons/src/main/java/tests/smoke/ContentStoreTests.java
@@ -1,21 +1,30 @@
 package tests.smoke;
 
-import examples.models.Claim;
-import examples.models.ClaimForm;
-import examples.repositories.ClaimRepository;
-import examples.stores.ClaimFormStore;
-import org.apache.commons.io.IOUtils;
-import org.junit.Assert;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.AfterEach;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.BeforeEach;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.Context;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.Describe;
+import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.It;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.content.commons.property.PropertyPath;
+
+import examples.models.Claim;
+import examples.models.ClaimForm;
+import examples.repositories.ClaimRepository;
+import examples.stores.ClaimFormStore;
 
 public abstract class ContentStoreTests extends AssociativeStoreTests {
 
@@ -106,6 +115,84 @@ public abstract class ContentStoreTests extends AssociativeStoreTests {
 						Assert.assertEquals(claim.getClaimForm().getContentLength(), 0);
 					});
 				});
+			});
+
+			Context("property path access", () -> {
+
+			    Context("given an Entity with content", () -> {
+
+			        BeforeEach(() -> {
+			            claim = new Claim();
+			            claim.setFirstName("John");
+			            claim.setLastName("Smith");
+			            claim.setClaimForm(new ClaimForm());
+			            claimFormStore.setContent(claim.getClaimForm(), PropertyPath.from("content"), new ByteArrayInputStream("Hello Spring Content World!".getBytes()));
+			            claim = claimRepo.save(claim);
+			        });
+
+			        It("should be able to store new content", () -> {
+			            boolean matches = false;
+			            InputStream content = null;
+			            try {
+			                content = claimFormStore.getContent(claim.getClaimForm(), PropertyPath.from("content"));
+			                matches = IOUtils.contentEquals(new ByteArrayInputStream("Hello Spring Content World!".getBytes()), content);
+			            } catch (IOException e) {
+			            } finally {
+			                IOUtils.closeQuietly(content);
+			            }
+			            assertThat(matches, is(true));
+			        });
+
+			        It("should have content metadata", () -> {
+			            Assert.assertThat(claim.getClaimForm().getContentId(), is(notNullValue()));
+			            Assert.assertThat(claim.getClaimForm().getContentId().trim().length(), greaterThan(0));
+			            Assert.assertEquals(claim.getClaimForm().getContentLength(), 27L);
+			        });
+
+			        Context("when content is updated", () -> {
+			            BeforeEach(() ->{
+			                claimFormStore.setContent(claim.getClaimForm(), PropertyPath.from("content"), new ByteArrayInputStream("Hello Updated Spring Content World!".getBytes()));
+			                claim = claimRepo.save(claim);
+			            });
+
+			            It("should have the updated content", () -> {
+			                boolean matches = false;
+			                InputStream content = null;
+			                try {
+			                    content = claimFormStore.getContent(claim.getClaimForm(), PropertyPath.from("content"));
+			                    matches = IOUtils.contentEquals(new ByteArrayInputStream("Hello Updated Spring Content World!".getBytes()), content);
+			                } catch (IOException e) {
+			                } finally {
+			                    IOUtils.closeQuietly(content);
+			                }
+			                assertThat(matches, is(true));
+			            });
+			        });
+
+			        Context("when content is deleted", () -> {
+			            BeforeEach(() -> {
+			                id = claim.getClaimForm().getContentId();
+			                claimFormStore.unsetContent(claim.getClaimForm(), PropertyPath.from("content"));
+			                claim = claimRepo.save(claim);
+			            });
+
+			            AfterEach(() -> {
+			                claimRepo.delete(claim);
+			            });
+
+			            It("should have no content", () -> {
+			                ClaimForm deletedClaimForm = new ClaimForm();
+			                deletedClaimForm.setContentId((String)id);
+
+			                InputStream content = claimFormStore.getContent(deletedClaimForm, PropertyPath.from("content"));
+			                Assert.assertThat(content, is(nullValue()));
+			                IOUtils.closeQuietly(content);
+
+			                Assert.assertThat(claim.getClaimForm().getContentId(), is(nullValue()));
+			                Assert.assertEquals(claim.getClaimForm().getContentLength(), 0);
+			            });
+			        });
+			    });
 			});
 		});
 	}
