4992c5608070206aba8dd0f05a14e251efed304b
Paul Warren <paul_c_warren@yahoo.com>
2022-04-26 22:21:20 -0700

    Remove rendition service, not required

--- spring-eg-content-renditions/src/test/java/api/java/RenditionTest.java
+++ spring-eg-content-renditions/src/test/java/api/java/RenditionTest.java
@@ -1,23 +1,25 @@
 package api.java;
 
-import examples.models.Claim;
-import examples.models.ClaimForm;
-import examples.repositories.ClaimRepository;
-import examples.stores.ClaimFormStore;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.InputStream;
+
 import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.content.commons.renditions.RenditionService;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import examples.models.Claim;
+import examples.models.ClaimForm;
+import examples.repositories.ClaimRepository;
+import examples.stores.ClaimFormStore;
 import tests.smoke.JpaConfig;
-
-import java.io.InputStream;
-
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { JpaConfig.class, RenditionTestConfig.class })
@@ -25,18 +27,15 @@ public class RenditionTest {
 
 	@Autowired
 	private ClaimRepository claimRepo;
-	
+
 	@Autowired
 	private ClaimFormStore claimFormStore;
-	
-	@Autowired
-	private RenditionService renditionService;
-	
+
     private Claim claim;
-    
+
     @Before
     public void setUp() throws Exception {
-    	
+
     	// create a claim that can get content from
     	claim = new Claim();
     	claim.setFirstName("John");
@@ -48,11 +47,6 @@ public class RenditionTest {
     	claimRepo.save(claim);
     }
 
-    @Test
-    public void testRenditionService() {
-    	assertThat(renditionService, is(not(nullValue())));
-    }
-    
     @Test
     public void testRendition() throws Exception {
     	InputStream is = claimFormStore.getRendition(claim.getClaimForm(), "text/plain");
